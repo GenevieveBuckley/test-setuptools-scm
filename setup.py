b95159203af0ad29a3a95cb42f2ac0a70407e3ef
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2022-09-01 20:29:08 +1000

    Add __version__ back into setup.py, will this break things?

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 
 from setuptools import setup, find_packages
 
+from myproj._version import __version__, __version_tuple__
 
 DISTNAME = "myproj"
 DESCRIPTION = (
@@ -12,6 +13,7 @@ PYTHON_VERSION = (3, 10)
 if __name__ == "__main__":
     setup(
         name=DISTNAME,
+        version=__version__,
         description=DESCRIPTION,
         author=MAINTAINER,
         packages=find_packages(),
